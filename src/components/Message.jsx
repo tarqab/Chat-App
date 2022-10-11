87467991886c640bbc3fb2a9da6c33d351610310
Tareq Gharibeh <tareq.gharibeh@stud.fra-uas.de>
2022-10-11 19:19:32 +0200

    changes

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,8 +2,8 @@ import { auth } from "../firebase";
 
 const style = {
   message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
-  userName: `absolute mt-[-4rem] text-gray-600 text-xs`,
-  sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full`,
+  userName: `text-white absolute mt-[-4rem]  text-xs`,
+  sent: `bg-blue-600 text-white flex-row-reverse text-end float-right rounded-bl-full`,
   received: `bg-[#e5e5ea] text-black float-left rounded-br-full`,
 };
 
